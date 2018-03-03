fea60242de1af25a34746bc82be58aa899817e7e
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-03-03 23:21:02 +0200

    addAdaptiveCheifUp

--- js/script.js
+++ js/script.js
@@ -27,3 +27,21 @@
     map: map
   });
 }
+ var overlay = document.getElementById('overlay');
+ var vid = document.getElementById('video');
+
+ if(overlay.addEventListener){
+     overlay.addEventListener("click", play, false)
+ }else if(overlay.attachEvent){
+     overlay.attachEvent("onclick", play)
+ }
+
+ function play() {
+     if (vid.paused){
+         vid.play();
+         overlay.className = "o";
+     }else {
+         vid.pause();
+         overlay.className = "";
+     }
+ }
